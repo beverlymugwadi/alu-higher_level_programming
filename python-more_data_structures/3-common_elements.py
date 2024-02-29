4df8313c8c6cfac3e252421ee67be74323b8f49e
beverlymugwadi <b.mugwadi1@alustudent.com>
2024-02-29 11:51:14 -0800

    Present in both

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-<<<<<<< HEAD
-    return (set_1 & set_2)
-=======
-    inter_set = set_1 & set_2
-    return inter_set#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    inter_set = set_1 & set_2
-    return inter_set#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    inter_set = set_1 & set_2
-    return inter_set
->>>>>>> 077c70476ceadeecf37df4288007ed4b778d3c26
+def raise_exception_msg(message=""):
+    raise NameError(message)
