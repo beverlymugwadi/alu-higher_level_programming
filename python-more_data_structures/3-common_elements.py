90c635f47a6a34eb4ccde3c728ea5b519a610d0d
beverlymugwadi <155968715+beverlymugwadi@users.noreply.github.com>
2024-02-29 14:37:01 +0000

    Data Structures: Set, Dictionary

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    inter_set = set_1 & set_2
+    return inter_set#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    inter_set = set_1 & set_2
+    return inter_set#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    inter_set = set_1 & set_2
+    return inter_set
