82756ef29204e7d26855366d34d4061ff32892aa
beverlymugwadi <b.mugwadi1@alustudent.com>
2024-02-29 06:00:19 -0800

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,5 @@
-#!/usr/bin/pythonk3
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-result = [row[i]**2 for row in matrix for i in range(3)]
-print(result)
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
+    return [[num * num for num in row] for row in matrix]
