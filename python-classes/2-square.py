99c0a2c5c80893931b9a0fd1504da0acbb6c77b9
beverlymugwadi <you@example.com>
2024-03-13 00:01:48 -0700

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-''' Create class Square '''
+"""Write a class Square that defines a square"""
+
 
 class Square:
-    ''' 
-    instantiate the class 
+    """Write a class Square that defines a square"""
 
-    '''
-
-
-    def __init__(self,size=0):
-        ''' init size '''
-        if not isinstance(size,int):
-            raise TypeError('size must be an integer')
+    def __init__(self, size=0):
+        """initialize square
+               Args:
+                   size (int): size of the square
+               """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
