976e586edc3749115389055718c31b3eb2728077
beverlymugwadi <you@example.com>
2024-03-13 00:09:55 -0700

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""Create a square """
+"""Write a class Square that defines a square"""
 
 
 class Square:
-    '''
-    Create a square
-        Has a private Instance att: size
-    '''
+    """Write a class Square that defines a square"""
 
     def __init__(self, size=0):
-        ''' init size '''
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        "returns the size att"
         return self.__size
 
     @size.setter
-    def size(self, size):
-        '''asign the size to the size att'''
-        if not isinstance(size, int):
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = value
 
     def area(self):
-        square_area = self.__size ** 2
-        return squ
+        return self.__size**2
