0eaa70393f61869322af88df779e631821e2a68d
beverlymugwadi <you@example.com>
2024-03-16 03:03:26 -0700

    Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that return boolean"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return true if obj s an instance else false"""
+    return True if isinstance(obj) else False
