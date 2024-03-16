6686280a3cfabae57338e0c701ba492b207cd9ca
beverlymugwadi <you@example.com>
2024-03-16 04:57:36 -0700

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,9 +6,9 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-
 class Rectangle(BaseGeometry):
     """Represent a rectnagle"""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
