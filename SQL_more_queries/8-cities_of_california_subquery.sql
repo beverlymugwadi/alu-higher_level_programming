b9da5bc0ac05771e5afe11dad0a1df1ba866fe83
beverlymugwadi <you@example.com>
2024-03-20 11:12:15 -0700

    Cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
---List cities
---List cities 
-SELECT name, id FROM cities WHERE state_id (SELECT id FROM states WHERE name = 'California' ORDER BY id;
+-- List cities in California
+SELECT name, id FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
