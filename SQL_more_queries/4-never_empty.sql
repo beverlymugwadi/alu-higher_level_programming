b93667f69481c9ede34a5ffb12648ff993877b6a
beverlymugwadi <you@example.com>
2024-03-20 11:01:08 -0700

    ID can't be null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
---Create a table
---Create table with constraints
-CREATE TABLE IF NOT EXIST id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+-- Create a table
+-- Create table with constraints
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
