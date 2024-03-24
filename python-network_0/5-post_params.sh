e3cb8d58b275fcc16625a6ba3ff474aa2679a32c
beverlymugwadi <b.mugwadi1@alustudent.com>
2024-03-24 14:45:48 +0000

    Network0

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#make a post request with email an subject as params
-curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# Make a POST request with email and subject as params
+
+# Check if the number of arguments is correct
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Assign URL to a variable
+URL="$1"
+
+# Make the POST request using curl
+curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$URL"
