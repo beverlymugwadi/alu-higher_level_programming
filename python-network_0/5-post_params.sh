31870c6e7a5d97879ff58f46728b77b08da27006
beverlymugwadi <b.mugwadi1@alustudent.com>
2024-03-24 17:42:26 +0000

    Network

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#make a post request with email an subject as params
-curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
